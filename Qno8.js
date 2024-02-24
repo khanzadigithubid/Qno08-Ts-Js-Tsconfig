d6c915e1837b133b8611e57c37478bc3b0857238
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:29:31 +0500

    Qno08 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno08 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno8.js
@@ -0,0 +1,12 @@
+"use strict";
+/* You should create four lines look like this:
+console.log(5+3);
+Your output should simply be four lines with the number 8 appearing once on each line*/
+//Addition
+console.log(6 + 2);
+//Subtraction
+console.log(10 - 2);
+//Multiplication
+console.log(4 * 2);
+//Division
+console.log(16 / 2);
